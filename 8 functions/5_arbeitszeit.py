ffbffc40ecc491ef4fa8cebd443a4093af7fa725
pyucation <114669795+pyucation@users.noreply.github.com>
2024-05-15 09:33:03 +0200

    updated task description

--- 8 functions/5_arbeitszeit.py
+++ 8 functions/5_arbeitszeit.py
@@ -4,7 +4,4 @@ wenn Startuhrzeit und Enduhrzeit gegeben ist. Verwende KEINE externen Module.
 
 Hinweis: Überlege dir, wie du die Uhrzeiten gut erfassen kannst, um leichte Rechnungen
 zu haben. Tipp: Du solltest 4 Parameter für deine Funktion haben.
-
-Hinweis: Die Funktion abs() berechnet den Absolut-Wert (Betrag) einer Zahl aus.
-Sie macht also aus einer negativen Zahl eine positive.
 """
