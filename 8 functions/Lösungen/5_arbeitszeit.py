d4090e81a18f235be780e62488b4e74d495e8ab1
pyucation <114669795+pyucation@users.noreply.github.com>
2024-05-15 09:33:36 +0200

    corrected wrong solution

--- 8 functions/Lösungen/5_arbeitszeit.py
+++ 8 functions/Lösungen/5_arbeitszeit.py
@@ -4,16 +4,18 @@ wenn Startuhrzeit und Enduhrzeit gegeben ist. Verwende KEINE externen Module.
 
 Hinweis: Überlege dir, wie du die Uhrzeiten gut erfassen kannst, um leichte Rechnungen
 zu haben. Tipp: Du solltest 4 Parameter für deine Funktion haben.
-
-Hinweis: Die Funktion abs() berechnet den Absolut-Wert (Betrag) einer Zahl aus.
-Sie macht also aus einer negativen Zahl eine positive.
 """
 
 # h - Stunde
 # m - Minute
 def arbeitszeit(h_start, m_start, h_ende, m_ende):
     delta_stunden = h_ende - h_start
-    delta_minuten = abs(m_ende - m_start)
+    # delta_minuten = abs(m_ende - m_start) # falsch!
+    if m_ende < m_start:
+        delta_minuten = 60 - m_start + m_ende
+    else:
+        delta_stunden += 1
+        delta_minuten = m_ende - m_start
     return f"{delta_stunden} h, {delta_minuten} m"
 
-print(arbeitszeit(7, 32, 17, 12))
+print(arbeitszeit(7, 12, 17, 32))
